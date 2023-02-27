4fbb3a186c60f4aeaee00f3d52d19ed0e6ad46fa
Akarsh Mishra <95975885+akarshmishra001@users.noreply.github.com>
2023-02-27 19:31:02 +0530

    fixed confirm pronunciation

--- src/components/confirmpronunciation/ConfirmPronTile.js
+++ src/components/confirmpronunciation/ConfirmPronTile.js
@@ -11,12 +11,9 @@ import TextField from "@mui/material/TextField";
 import Pagination from "@mui/material/Pagination";
 import ColorCheckboxes from "../CheckBoxPick.js/ColorCheckboxes";
 import { useQuery, useQueryClient } from "react-query";
-
-const username = localStorage.getItem("username");
-const socketUrl = `${WEB_BASE_URL}/audiomis.io/`;
+import ConfirmPronRow from "./ConfirmPronRow";
 
 const ConfirmPronTile = () => {
-  const [emittedData, setemittedData] = useState("");
   const queryClient = useQueryClient();
   // const [status, setStatus] = useState("");
   // const [option, setOptions] = useState("");
@@ -24,34 +21,9 @@ const ConfirmPronTile = () => {
   const [pageNumber, setPageNumber] = useState(1);
   const accessToken = localStorage.getItem("authToken");
 
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
-    onMessage: (message) => {
-      const data = JSON.parse(message?.data);
-      setemittedData(JSON.parse(data?.data));
-    },
-  });
 
-  const handleClickAndSendMessage = useCallback(
-    (payload) => {
-      if (payload.true) {
-        sendMessage(
-          JSON.stringify({
-            user: username,
-            true: true,
-            ...payload,
-          })
-        );
-      } else {
-        sendMessage(
-          JSON.stringify({
-            user: username,
-            ...payload,
-          })
-        );
-      }
-    },
-    [username]
-  );
+
+
 
   // const handleStatus = (event) => {
   //   setStatus(event.target.value);
@@ -81,8 +53,8 @@ const ConfirmPronTile = () => {
 
   let UpdateConfirmName = async (buttonPressed, engName, videoId) => {
     try {
-      fetch(`${BASE_URL}/audio/updt-redo-lip-newnamecode`, {
-        method: "PUT",
+      fetch(`${BASE_URL}/audio/update-confirm-pronunciation`, {
+        method: "POST",
         body: JSON.stringify({
           englishName: engName,
           buttonPressed: buttonPressed,
@@ -133,101 +105,7 @@ const ConfirmPronTile = () => {
         </div>
       </div>
       {audioConfirmPro?.map((value, index) => (
-        <div key={`${value}-${index}`} className="au-mt">
-          <div className="main-tile">
-            <ColorCheckboxes
-              tileName={value}
-              handleClickAndSendMessage={handleClickAndSendMessage}
-              />
-            <div className="main-tile-head">
-              <Typography
-                className="video-name"
-                sx={{
-                  paddingLeft: "1rem",
-                }}
-                >
-                {value}
-              </Typography>
-              {emittedData &&
-                JSON.parse(emittedData)?.filter(
-                  (data) => data?.video_id === value
-                )?.length > 0 && (
-                  <Chip
-                    label={`In progress: ${
-                      JSON.parse(emittedData)?.filter(
-                        (data) => data?.video_id === value
-                      )?.[0]?.user
-                    }`}
-                    sx={{ ml: "5px", backgroundColor: "white" }}
-                  ></Chip>
-                )}
-            </div>
-          </div>
-          <div className="main-tiles">
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": { m: 1, width: "25ch" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                sx={{
-                  width: { sm: 200, md: 300 },
-                  "& .MuiInputBase-root": {
-                    width: 250,
-                  },
-                }}
-                id="outlined-basic"
-                label="English Name"
-                variant="outlined"
-              />
-            </Box>
-            <Button
-              variant="contained"
-              onClick={() => {
-                UpdateConfirmName(
-                  "Refunded",
-                  value.split("_")[3].split(".")[0]
-                );
-              }}
-              sx={{
-                height: "2.5rem",
-                backgroundColor: "#D7B8FD",
-                color: "white",
-                "&:hover": {
-                  backgroundColor: "#ad6efb",
-                  color: "#fff",
-                },
-              }}
-            >
-              Refunded
-            </Button>
-            <Button
-              onClick={() => {
-                UpdateConfirmName(
-                  "Confirm Name",
-                  value.split("_")[3].split(".")[0]
-                );
-              }}
-              variant="contained"
-              // disabled={isDisabled}
-              sx={{
-                height: "2.5rem",
-                // marginTop: ".46rem",
-                backgroundColor: "#D7B8FD",
-                color: "white",
-                "&:hover": {
-                  backgroundColor: "#ad6efb",
-                  color: "#fff",
-                },
-              }}
-            >
-              Confirmed
-            </Button>
-          </div>
-        </div>
+        <ConfirmPronRow key={`${value}-${index}`} value={value} />
       ))}
     </div>
   );
