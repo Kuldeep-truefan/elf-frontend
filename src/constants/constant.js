09ff21bcbec4a74b9ef8ad8258ebb4c5ad1bd415
trueanshul <anshul.innovatios@gmail.com>
2023-02-27 20:32:55 +0530

    Password issue1

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -2,14 +2,14 @@
 // export const BASE_URL = "http://34.29.72.93:8000"
 // export const WEB_BASE_URL = "ws://34.29.72.93:8000"
 
-export const BASE_URL = "http://127.0.0.1:8000"
-export const WEB_BASE_URL = "ws://127.0.0.1:8000"
+// export const BASE_URL = "http://127.0.0.1:8000"
+// export const WEB_BASE_URL = "ws://127.0.0.1:8000"
 
 
 // dev servers
 
-// export const BASE_URL = "http://34.29.72.93:8001"
-// export const WEB_BASE_URL = "ws://34.29.72.93:8001"
+export const BASE_URL = "http://34.29.72.93:8001"
+export const WEB_BASE_URL = "ws://34.29.72.93:8001"
 
 // export const BASE_URL = "http://127.0.0.1:8001"
 // export const WEB_BASE_URL = "ws://127.0.0.1:8001"
