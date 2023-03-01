39f373476a9fe2b6d47210abab69ac7c475f8aea
trueanshul <anshul.innovatios@gmail.com>
2023-03-01 12:08:25 +0530

    Constants

--- src/components/db/PopUp.js
+++ src/components/db/PopUp.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import React, { useState } from "react";
+import { useState } from "react";
 
 
 export default function PopUp() {
