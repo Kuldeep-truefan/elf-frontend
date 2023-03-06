cc04fb37e46dbc2817234b019839f32bff967ef3
trueanshul <anshul.innovatios@gmail.com>
2023-03-06 19:21:21 +0530

    autofill feature

--- src/components/redolipsync/RedoLipRowTile.js
+++ src/components/redolipsync/RedoLipRowTile.js
@@ -5,156 +5,10 @@ import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 import { BASE_URL, WEB_BASE_URL } from "../../constants/constant";
 import ColorCheckboxes from "../CheckBoxPick.js/ColorCheckboxes";
 import RedoLipModal from "./RedoLipModal";
-import Autocomplete from '@mui/material/Autocomplete';
-
-
-const username = localStorage.getItem("username");
-const socketUrl = `${WEB_BASE_URL}/ausoket.io/`;
-const accessToken = localStorage.getItem("authToken");
-
-const RedoLipRowTile = ({ tileName, comments, nameCode, pageNumber }) => {
-  const queryClient = useQueryClient();
-  const [emittedData, setemittedData] = useState("");
-  const [open, setOpen] = useState(false);
-  const [newNameCode, setNewNameCode] = useState("");
-  const [inputValue, setInputValue] = React.useState('');
-  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
-    onMessage: (message) => {
-      const data = JSON.parse(message?.data);
-      setemittedData(JSON.parse(data?.data));
-    },
-  });
-  const handleChange = (event, newValue) => {
-    console.log(newValue, 'Namecode')
-    setNewNameCode(newValue);
-  };
-  const handleClickAndSendMessage = useCallback(
-    (payload) =>
-      sendMessage(
-        JSON.stringify({
-          user: username,
-          ...payload,
-        })
-      ),
-    [username]
-  );
-  const handelClick = () => {
-    setOpen(open);
-  };
-  let UpdateRedoLipSync = async (videoId) => {
-    try {
-      fetch(`${BASE_URL}/audio/updt-redo-lip-newnamecode`, {
-        method: "PUT",
-        body: JSON.stringify({
-          newNameCode: newNameCode,
-          videoId: videoId,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-        .then(() => {
-          queryClient.invalidateQueries(["FetchAudioRedoLipSync", pageNumber]);
-        })
-        .then((data) => {});
-    } catch (error) {
-      console.log("Error occured", error);
-    }
-  };
-  return (
-    <div className="au-mt">
-      <div className="main-tile">
-        <ColorCheckboxes
-          tileName={tileName}
-          handleClickAndSendMessage={handleClickAndSendMessage}
-        />
-        <div className="main-tile-head">
-          <Typography
-            className="video-name"
-            sx={{
-              paddingLeft: "1rem",
-            }}
-          >
-            {tileName}
-          </Typography>
-          {emittedData &&
-            JSON.parse(emittedData)?.filter(
-              (data) => data?.video_id === tileName
-            )?.length > 0 && (
-              <Chip
-                label={`In progress: ${
-                  JSON.parse(emittedData)?.filter(
-                    (data) => data?.video_id === tileName
-                  )?.[0]?.user
-                }`}
-                sx={{ ml: "5px", backgroundColor: "white" }}
-              ></Chip>
-            )}
-        </div>
-        <p className="video-name-dynamic">{comments}</p>
-      </div>
-      <div className="main-tiles">
-        <RedoLipModal
-          onClick={handelClick}
-          open={open}
-          setOpen={setOpen}
-          item={tileName}
-        />
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            value={nameCode}
-            id="outlined-basic"
-            disabled="True"
-            wrap="hard"
-            variant="outlined"
-          />
-          <Autocomplete
-            disablePortal
-            id="outlined-basic"
-            options={namecodes}
-            sx={{ width: 300 }}
-            inputValue={newNameCode}
-            onInputChange={handleChange}
-            renderInput={(params) => <TextField {...params} label="Select Namecode" />}
-          />
-          {/* <TextField
-            options={namecodes}
-            id="outlined-basic"
-            label="Type Namecode"
-            variant="outlined"
-            onChange={handleChange}
-          /> */}
-        </Box>
-        <Button
-          onClick={() => {
-            UpdateRedoLipSync(tileName.split("_")[3].split(".")[0]);
-          }}
-          variant="contained"
-          sx={{
-            height: "2.5rem",
-            backgroundColor: "#D7B8FD",
-            color: "white",
-            "&:hover": {
-              backgroundColor: "#ad6efb",
-              color: "#fff",
-            },
-          }}
-        >
-          Done
-        </Button>
-      </div>
-    </div>
-  );
-};
-
+import AutoComplete from '../auto-complete/AutoComplete'
+import { createFilterOptions } from "@mui/material/Autocomplete";
+import { useMemo } from "react";
+import { debounce } from "lodash";
 const namecodes = [
   "a",
   "d",
@@ -7505,4 +7359,169 @@ const namecodes = [
   "wiwa",
   "yobe",
 ];
+const filterOptions = createFilterOptions({
+  matchFrom: 'any',
+  limit: 500,
+});
+const debouncedHandleChange = debounce((newValue, setNewNameCode) => {
+  console.log(newValue, "Namecode");
+  setNewNameCode(newValue);
+}, 500);
+
+const username = localStorage.getItem("username");
+const socketUrl = `${WEB_BASE_URL}/ausoket.io/`;
+const accessToken = localStorage.getItem("authToken");
+
+const RedoLipRowTile = ({ tileName, comments, nameCode, pageNumber }) => {
+  const queryClient = useQueryClient();
+  const [emittedData, setemittedData] = useState("");
+  const [open, setOpen] = useState(false);
+  const [newNameCode, setNewNameCode] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, {
+    onMessage: (message) => {
+      const data = JSON.parse(message?.data);
+      setemittedData(JSON.parse(data?.data));
+    },
+  });
+  const memoizedOptions = namecodes
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue, 'newValue');
+    setNewNameCode(newValue)
+    // debouncedHandleChange(newValue, setNewNameCode);
+  };
+  const handleClickAndSendMessage = useCallback(
+    (payload) =>
+      sendMessage(
+        JSON.stringify({
+          user: username,
+          ...payload,
+        })
+      ),
+    [username]
+  );
+  const handelClick = () => {
+    setOpen(open);
+  };
+  let UpdateRedoLipSync = async (videoId) => {
+    try {
+      fetch(`${BASE_URL}/audio/updt-redo-lip-newnamecode`, {
+        method: "PUT",
+        body: JSON.stringify({
+          newNameCode: newNameCode,
+          videoId: videoId,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+        .then(() => {
+          queryClient.invalidateQueries(["FetchAudioRedoLipSync", pageNumber]);
+        })
+        .then((data) => {});
+    } catch (error) {
+      console.log("Error occured", error);
+    }
+  };
+  return (
+    <div className="au-mt">
+      <div className="main-tile">
+        <ColorCheckboxes
+          tileName={tileName}
+          handleClickAndSendMessage={handleClickAndSendMessage}
+        />
+        <div className="main-tile-head">
+          <Typography
+            className="video-name"
+            sx={{
+              paddingLeft: "1rem",
+            }}
+          >
+            {tileName}
+          </Typography>
+          {emittedData &&
+            JSON.parse(emittedData)?.filter(
+              (data) => data?.video_id === tileName
+            )?.length > 0 && (
+              <Chip
+                label={`In progress: ${
+                  JSON.parse(emittedData)?.filter(
+                    (data) => data?.video_id === tileName
+                  )?.[0]?.user
+                }`}
+                sx={{ ml: "5px", backgroundColor: "white" }}
+              ></Chip>
+            )}
+        </div>
+        <p className="video-name-dynamic">{comments}</p>
+      </div>
+      <div className="main-tiles">
+        <RedoLipModal
+          onClick={handelClick}
+          open={open}
+          setOpen={setOpen}
+          item={tileName}
+        />
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            value={nameCode}
+            id="outlined-basic"
+            disabled="True"
+            wrap="hard"
+            variant="outlined"
+          />
+          <AutoComplete
+          options={memoizedOptions}
+          onChange={handleChange}
+          inputValue={newNameCode}
+          //  filterOptions={filterOptions}
+          //   disableListWrap
+          //   disablePortal
+            
+          //   options={memoizedOptions}
+          //   sx={{ width: 300 }}
+          //   renderInput={(params) => (
+          //     <TextField {...params} label="Select Namecode" />
+          //   )}
+          />
+          {/* <TextField
+            options={namecodes}
+            id="outlined-basic"
+            label="Type Namecode"
+            variant="outlined"
+            onChange={handleChange}
+          /> */}
+        </Box>
+        <Button
+          onClick={() => {
+            UpdateRedoLipSync(tileName.split("_")[3].split(".")[0]);
+          }}
+          variant="contained"
+          sx={{
+            height: "2.5rem",
+            backgroundColor: "#D7B8FD",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "#ad6efb",
+              color: "#fff",
+            },
+          }}
+        >
+          Done
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+
 export default RedoLipRowTile;
