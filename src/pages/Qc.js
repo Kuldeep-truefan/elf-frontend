08c0d6bfabdef028721b2300703d84a1b1f89dac
Kuldeep Rawat <kuldeep.rawat@true-fan.in>
2023-03-14 16:37:04 +0530

    dropdown for video mode and audio in simplified names page added with the api call

--- src/pages/Qc.js
+++ src/pages/Qc.js
@@ -117,6 +117,7 @@ function Qc() {
                     setPageCount={setPageCount}
                     setLoadbucket={setLoadbucket}
                     fetchLinkMutate={fetchLinkMutate}
+                    loadbucket={loadbucket}
                   />
                     
             </div>
